b61a3366488dc375a958139a12961a1c4b14a5ff
Lorrami <r.riabinin.ik12@kpi.ua>
2022-02-10 19:07:56 +0200

    Little update

--- sources/Level.cpp
+++ sources/Level.cpp
@@ -2,7 +2,6 @@
 
 #include "Level.hpp"
 #include "GameObject.hpp"
-#include <iostream>
 
 Level::Level()
 {
@@ -23,7 +22,7 @@ void Level::Update(float dt)
         Object->Update(dt);
 		for (GameObject* NextObject : m_Objects)
 		{
-			//if (Object != NextObject && Object->getGlobalBounds().findIntersection(NextObject->getGlobalBounds()))
+			if (Object != NextObject && Object->FindIntersection()
 			{
 				Object->OnColliderEnter(NextObject);
 			}
